900840b7f5ade36ad0ddf643ccafbf18c3032862
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-23 18:06:12 +0300

    done

--- day7-MovieApp/day7-MovieApp/ContentView.swift
+++ day7-MovieApp/day7-MovieApp/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let movies = ["interstaller", "knives out", "sherlok holmes"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List(movies, id: \.self){ movie in
+            ExtractedView(movie: movie)
+            
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct ExtractedView: View {
+    var movie: String
+    var body: some View {
+        HStack{
+            Image(movie)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+            
+            Text(movie)
+            
+        }
     }
 }
